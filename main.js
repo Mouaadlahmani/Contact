09bf6440024cabc8f191936ce9b46c4c02c946d1
Mouaadlahmani <mouaadlahmani@gmail.com>
2024-03-28 15:29:12 +0000

    modification dans la search bar

---
+++ main.js
@@ -0,0 +1,15 @@
+// fetch('https://randomuser.me/api/?results=5').then((result) => {
+//     // console.log(result);
+//     let myData = result.json();
+//     // console.log(myData);
+//     return myData;
+// }).then((myData) => {
+//     myData.length = 5;
+//     return myData;
+// }).then((myData) => {
+//     console.log(myData);
+//     console.log(myData.results[0].picture);
+//     console.log(myData.results[0].email);
+//     console.log(myData.results[0].photo);
+//     console.log(myData.results[0].phone);
+// });
